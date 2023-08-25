f6822a093b02b3bc34a1644c2561370a41859117
Fernando Gonzalez <lufergonro@gmail.com>
2023-08-25 15:46:26 -0600

    cambios finales 25-08-2023

--- CalcIMC.py
+++ CalcIMC.py
@@ -4,51 +4,51 @@ while True:
     print('------- CALCULADORA DE IMC -------')
 
     while True:
-        nombre = input('Ingrese su Nombre: ').strip() #Método .strip() para eliminar los espacios que existan
-        if nombre.strip() and nombre.isalpha():  # Verifica si la variable no está vacía con .strip() y que sean solo letras con .isalpha()
+        nombre = input('Ingrese su Nombre: ').strip() #Método .strip() se utiliza para eliminar espacios en blanco al principio y al final de la entrada.
+        if nombre and nombre.replace(" ", "").isalpha():  # Verifica si la versión del nombre sin espacios (usando .replace()) solo son letras (usando .isalpha()).
             break  # Sale del bucle si se ingresa un valor válido
         else:
-            print("****** ¡Error! Ingresa un valor válido *****")
-
+            print("****** ¡Error! Ingresa un valor válido ******") #Muestra mensaje de error si la validación no es correcta
+   
     while True:
         apePaterno = input('Ingrese su Apellido Paterno: ').strip()
-        if apePaterno.strip() and apePaterno.isalpha():  # Verifica si la variable no está vacía con .strip() y que sean solo letras con .isalpha()
-            break  # Sale del bucle si se ingresa un valor válido
+        if apePaterno and apePaterno.replace(" ", "").isalpha():
+            break
         else:
-            print("****** ¡Error! Ingresa un valor válido *****")
+            print("****** ¡Error! Ingresa un valor válido ******")
 
     while True:
         apeMaterno = input('Ingrese su Apellido Materno: ').strip()
-        if apeMaterno.strip() and apeMaterno.isalpha():  # Verifica si la variable no está vacía con .strip() y que sean solo letras con .isalpha()
-            break  # Sale del bucle si se ingresa un valor válido
+        if apeMaterno and apeMaterno.replace(" ", "").isalpha():
+            break
         else:
-            print("****** ¡Error! Ingresa un valor válido *****")
+            print("****** ¡Error! Ingresa un valor válido ******")
 
     while True:
         try:
             edad = int(input('Ingrese su edad en años: '))
             break
         except ValueError:
-            print("****** ¡Error! Ingresa un valor válido *****")
+            print("****** ¡Error! Ingresa un valor válido ******")
 
     while True:
         try:
             peso = float(input('Ingrese su peso en kg: '))
             break
         except ValueError:
-            print("****** ¡Error! Ingresa un valor válido *****")
+            print("****** ¡Error! Ingresa un valor válido ******")
 
     while True:
         try:
             estatura = float(input('Ingrese su estatura en metros: '))
             break
         except ValueError:
-            print("****** ¡Error! Ingresa un valor válido *****")
+            print("****** ¡Error! Ingresa un valor válido ******")
 
-    IMC = peso / estatura**2
+    IMC = peso / estatura**2 #Calculando el IMC con la fórmula  Peso / estatura2
 
 
-    #****** Se compara el IMC para ver su condición ******
+    #****** Se compara el IMC para ver su condición de peso ******
     if IMC <= 18.9 :
         mensaje = 'Peso Bajo'
     elif IMC >= 19.00 and IMC <= 24.99 :
@@ -65,7 +65,7 @@ while True:
     #******Se imprimen los datos de la persona******
     print(f""" 
     ************** DATOS **************
-    Nombre: {nombre} {apePaterno} {apeMaterno}
+    Nombre: {nombre.title()} {apePaterno.title()} {apeMaterno.title()}
     Edad: {edad} años
     Estatura: {estatura}m
     Peso: {peso}kg
@@ -75,6 +75,7 @@ while True:
     Condición: {mensaje.upper()}
     """)
 
+    #Se pregunta si se quiere calcular otro IMC
     respuesta = input("¿Deseas calcular otro IMC? (si/no): ").strip().lower()
     if respuesta != "si" and respuesta != "s":
         print("¡Hasta luego!")
